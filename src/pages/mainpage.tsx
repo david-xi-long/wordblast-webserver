082200fe0f72fca6570f762c04a82e7a2dbbdefa
ANDREWZ\andre <andrewzheng8@gmail.com>
2021-10-07 01:18:37 -0400

    fix build error

--- src/pages/mainpage.tsx
+++ src/pages/mainpage.tsx
@@ -3,12 +3,12 @@ import {useState, useEffect} from 'react';
 function MainPage() {
     const [displayName, setDisplayName] = useState('');
 
-    //sign in the user. should store the player's username and session ID to the backend and database
+    // sign in the user. should store the player's username and session ID to the backend and database
     const signIn = () => {
         console.log("Authenticating User", displayName);
     }
     
-    //handle pressing 'enter' to enter game
+    // handle pressing 'enter' to enter game
     const submitForm = (e) => {
         if (e.keyCode === 13) {
             signIn()
@@ -19,13 +19,13 @@ function MainPage() {
         <div className="mainpage">
             <code style={{fontSize: '48px'}}>wordblast.io</code>
             <br/>
-            <code>enter your username and hit 'enter' to play!</code>
+            <code>enter your username and hit &apos;enter&apos; to play!</code>
             <br/>
             <input className="username" placeholder="Enter a username..." onKeyDown={(e) => submitForm(e)} value={displayName} onChange={(e) => setDisplayName(e.target.value)}/>
             <br/>
             <div className="row">
-                <button className="login-button" onClick={signIn}>play</button>
-                <button className="login-button" onClick={signIn}>play with friends</button>
+                <button type="submit" onClick={signIn}>play</button>
+                <button type="submit" onClick={signIn}>play with friends</button>
             </div>
         </div>
         
