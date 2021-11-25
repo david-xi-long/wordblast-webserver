fcf817125979c5c6f8ba3e6f70558fdae17de2a1
Emilio Barradas <decalerss@gmail.com>
2021-11-25 13:31:12 -0500

    Cleanup

--- src/components/game/GameplayScreen.tsx
+++ src/components/game/GameplayScreen.tsx
@@ -11,32 +11,6 @@ import PacketInDefinition from '../../scripts/packets/in/PacketInDefinition';
 import Countdown from './Countdown';
 import GameplayPlayerSlots from './GameplayPlayerSlots';
 
-// maps out the word the current player is typing character by character and
-// colors in the letter combination green.
-function Word(props) {
-    const { word, letterCombo } = props;
-    const wordArray = [...word];
-    const sIndex = word.indexOf(letterCombo.toString());
-    const eIndex = sIndex + letterCombo.length;
-
-    return (
-        <div style={{ display: 'flex', justifyContent: 'center' }}>
-            {wordArray.map((w, i) => {
-                if (sIndex === -1) return <p key={Math.random()}>{w}</p>;
-
-                if (i >= sIndex && i < eIndex)
-                    return (
-                        <p key={Math.random()} style={{ color: 'lightgreen' }}>
-                            {w}
-                        </p>
-                    );
-
-                return <p key={Math.random()}>{w}</p>;
-            })}
-        </div>
-    );
-}
-
 const GameplayPage: NextPage<{
     gameSocket: GameSocket;
     players: Player[];
@@ -44,59 +18,10 @@ const GameplayPage: NextPage<{
     roundInfo: RoundInfo;
     username: string;
     gameId: string;
-<<<<<<< HEAD
 }> = ({ gameSocket, players, setPlayers, roundInfo, username, gameId }) => {
     const [eliminated, setEliminated] = useState(false);
 
     const registerInitHandlers = () => {
-=======
-<<<<<<< HEAD
-}> = ({ gameSocket, players, setPlayers, roundInfo, username, gameId }) => {
-    const [eliminated, setEliminated] = useState(false);
-
-    const registerInitHandlers = () => {
-=======
-}> = ({ gameSocket, players, roundInfo, username, gameId }) => {
-    const [curPlayerIndex, setCurPlayerIndex] = useState(0);
-    const [word, setWord] = useState('' as string);
-    const [timeLeft, setTimeLeft] = useState(1);
-    const [eliminated, setEliminated] = useState(false);
-
-    // I was too tired to figure out how to actually send a map over so I did it this way. It's ugly, I know.
-    // TODO: Map<String, Integer> -> { [playerName: string]: number }
-    const playerList = roundInfo.players;
-    const { playerLives } = roundInfo;
-    const playerMap = new Map();
-    for (let i = 0; i < playerList.length; i += 1) {
-        playerMap.set(playerList[i], playerLives[i]);
-    }
-
-    const updateWord = async (e) => {
-        gameSocket.fireAndForget(
-            'update-word',
-            new PacketOutPlayerMessage(gameId, username, e.target.value)
-        );
-    };
-
-    useEffect(() => {
-        setCurPlayerIndex(
-            rotationIndexPositions[
-                players.findIndex((p) => p.username === roundInfo.username)
-            ]
-        );
-    }, [roundInfo, players]);
-
-    // set timer each with each new round
-    useEffect(() => {
-        setWord('');
-        // delay to allow 'out of time' message
-        setTimeout(() => {}, 1000);
-        setTimeLeft(Math.round(roundInfo.timeRemaining / 1000) - 1);
-    }, [roundInfo]);
-
-    useEffect(() => {
->>>>>>> fc3dee96e59c682db9f78b449654008a917e8a12
->>>>>>> 106f1b1451907ab985d143ed13f83e594ab112e8
         gameSocket.subscribe<PacketInPlayerEliminated>(
             'player-eliminated',
             (packet) => {
@@ -105,9 +30,6 @@ const GameplayPage: NextPage<{
             }
         );
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
         gameSocket.subscribe<PacketInDefinition>('definition', (packet) => {
             // alert("WORD:" + packet.getWord() + "Definition: " + packet.getDefinition());
         });
@@ -115,29 +37,6 @@ const GameplayPage: NextPage<{
 
     useEffect(() => {
         registerInitHandlers();
-=======
-        gameSocket.subscribe<PacketInPlayerMessage>('update-word', (packet) => {
-            setWord(packet.getMessage());
-        });
-
->>>>>>> 106f1b1451907ab985d143ed13f83e594ab112e8
-        gameSocket.subscribe<PacketInDefinition>('definition', (packet) => {
-            // alert("WORD:" + packet.getWord() + "Definition: " + packet.getDefinition());
-        });
-    };
-
-<<<<<<< HEAD
-    useEffect(() => {
-        registerInitHandlers();
-=======
-        // decrement the timer
-        setInterval(() => {
-            if (timeLeft > 0) {
-                setTimeLeft((time) => time - 1);
-            }
-        }, 1000);
->>>>>>> fc3dee96e59c682db9f78b449654008a917e8a12
->>>>>>> 106f1b1451907ab985d143ed13f83e594ab112e8
     }, []);
 
     const updateWord = async (e) => {
@@ -159,23 +58,12 @@ const GameplayPage: NextPage<{
                     // TODO: Implement word being correct
                 } else {
                     console.log('word', guess, 'is invalid');
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-                    setWord('');
->>>>>>> fc3dee96e59c682db9f78b449654008a917e8a12
->>>>>>> 106f1b1451907ab985d143ed13f83e594ab112e8
                     // TODO: Implement word being incorrect
                 }
             });
     };
 
     return (
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 106f1b1451907ab985d143ed13f83e594ab112e8
         <div className="p-8 pb-0 h-screen flex flex-col justify-center items-center">
             <div className="m-8 mb-16 h-24 flex flex-col justify-center items-center overflow-hidden">
                 <p className="text-4xl font-bold">
@@ -191,52 +79,6 @@ const GameplayPage: NextPage<{
                 roundInfo={roundInfo}
                 players={players}
                 setPlayers={setPlayers}
-<<<<<<< HEAD
-=======
-            />
-
-            <Input
-                className="mx-8 mt-16 mb-12 game-input"
-                style={{
-                    visibility:
-                        roundInfo.username === username ? 'initial' : 'hidden',
-                }}
-                onKeyDown={(e) => {
-                    if (e.key !== 'Enter') return;
-                    sendWordGuess(e.currentTarget.value);
-                }}
-                onChange={(e) => updateWord(e)}
-                onInput={(e) => {
-                    e.currentTarget.value =
-                        `${e.currentTarget.value}`.toUpperCase();
-                }}
-            />
-
-            <Countdown roundInfo={roundInfo} />
-=======
-        <div className="p-8 h-screen flex justify-center items-center">
-            <CircleSlots
-                middle={
-                    <Bomb
-                        deg={45 * curPlayerIndex - 45}
-                        combo={roundInfo.letterCombo}
-                        timeLeft={timeLeft}
-                    />
-                }
-                items={players.map((p) => ({ uid: p.username, ...p }))}
-                map={(player) => (
-                    <div className="font-semibold truncate center">
-                        {player.username}
-                        <p>Lives: {playerMap.get(player.username)}</p>
-                        {roundInfo.username === player.username && (
-                            <Word
-                                word={word}
-                                letterCombo={roundInfo.letterCombo}
-                            />
-                        )}
-                    </div>
-                )}
->>>>>>> 106f1b1451907ab985d143ed13f83e594ab112e8
             />
 
             <Input
