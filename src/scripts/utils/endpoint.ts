e10880bb1695e6803e0d8545b2e66ac814c347cb
Emilio Barradas <decalerss@gmail.com>
2021-12-07 23:28:27 -0500

    Use the correct environment endpoints.

--- src/scripts/utils/endpoint.ts
+++ src/scripts/utils/endpoint.ts
@@ -1,6 +1,6 @@
 const getGameEndpoint = () =>
     process.env.NODE_ENV === 'development'
         ? 'http://localhost:8080'
-        : 'http://34.145.177.221';
+        : 'http://34.145.177.221:8080';
 
 export default getGameEndpoint;
